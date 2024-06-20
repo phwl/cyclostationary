8a522201be5c94267eb083c81db361118b774cbc
Philip Leong <philp.leong@sydney.edu.au>
2024-06-21 09:08:32 +1000

    add commend line

--- analysis/src/torture.py
+++ analysis/src/torture.py
@@ -1,9 +1,10 @@
 # Import functions and libraries
+import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 # import pyaudio
 import threading,time
-import sys
 import cyclostationary
 import datetime
 
@@ -89,11 +90,11 @@ def gen_bpsk(Nbits, snrdB=100, sigdB=0, verbose=0):
     BPSK_s = sscale * BPSK + nscale * np.random.normal(0,1,len(imp_sinc))
     BPSK_s = BPSK_s / max(BPSK_s)
     if verbose & 1:
-        plot(t,BPSK_s)
+        plt.plot(t,BPSK_s)
     return t, BPSK_s
 
 # sweeps the noise level and returns the largest values of the alpha profile
-def bpsk_sweepsnr(Nbits, startdB=10, verbose=0):
+def bpsk_sweepsnr(Nbits, startSNR=10, verbose=0):
     # returns the set containing the peaks largest indices for the alpha profile for BPSK there should be 2 peaks
     def genandplot(snrdB, peaks=2):
         t, BPSK_s = gen_bpsk(Nbits, snrdB, verbose=verbose)
@@ -101,7 +102,7 @@ def bpsk_sweepsnr(Nbits, startdB=10, verbose=0):
         return set(np.argpartition(alpha, len(alpha)-peaks)[-peaks:]), scd
     
     resultl = []
-    testdB = startdB
+    testdB = startSNR
     mindex, origscd = genandplot(testdB)
     if verbose > 0:
         print(f'scd.shape={origscd.shape}')
@@ -115,19 +116,31 @@ def bpsk_sweepsnr(Nbits, startdB=10, verbose=0):
     genandplot(testdB)
     return testdB, resultl
 
-def runtest(start, stop, startdB=10):
-    verbose=0
+def runtest(start, stop, startSNR=10, verbose=0):
     scores = []
     for i in range(start, stop):
         bits = 1 << i
         start = datetime.datetime.now()
         try:
-            testdB, resultl = bpsk_sweepsnr(bits, startdB=testdB+3, verbose=verbose)
+            testdB, resultl = bpsk_sweepsnr(bits, startSNR=testdB+3, verbose=verbose)
         except:
-            testdB, resultl = bpsk_sweepsnr(bits, startdB=startdB, verbose=verbose)        
+            testdB, resultl = bpsk_sweepsnr(bits, startSNR=startSNR, verbose=verbose)        
         print(f'bits=2^{i}: Score_SNR={testdB} ({datetime.datetime.now() - start})', flush=True)
         scores.append([testdB, resultl])
     return scores
 
 if __name__ == '__main__':
-    scores = runtest(1,16,startdB=5)
+    if len(sys.argv) <= 1:
+        print(f'scores = runtest(1,16,5)')
+        scores = runtest(1,16,startSNR=5)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("start", type=int, help="start nbits e.g. 1")
+    parser.add_argument("stop", type=int, help="stop nbits e.g. 10")
+    parser.add_argument("startSNR", type=int, help="startSNR e.g. 0")
+    args = parser.parse_args()
+                
+    print(f'scores = runtest({args.start},{args.stop},startSNR={args.startSNR})')
+    scores = runtest(args.start, args.stop, 
+                    startSNR=args.startSNR, verbose=args.verbose)
